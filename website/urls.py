43d0c751bb6b36c6c34bbe802aac5e28f6644684
Edghi <ghislain.makolle@facsciences-uy1.cm>
2022-05-06 21:14:42 +0100

    Adding page names, configuring views and urls

--- website/urls.py
+++ website/urls.py
@@ -1,4 +1,12 @@
 from django.urls import path
 
+from .views import Accueil, Contact, Developpement, Hebergement, Marketing
 
-urlpatterns = []
+
+urlpatterns = [
+    path('', Accueil.as_view(), name='accueil'),
+    path('contact/', Contact.as_view(), name='contact'),
+    path('services/developpement/', Developpement.as_view(), name='developpement'),
+    path('services/hebergement/', Hebergement.as_view(), name='hebergement'),
+    path('marketing/', Marketing.as_view(), name='marketing'),
+]
--- website/views.py
+++ website/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class Accueil(TemplateView):
+    template_name = "website/accueil.html"
+
+
+class Contact(TemplateView):
+    template_name = "website/contact.html"
+
+
+class Developpement(TemplateView):
+    template_name = "website/developpement.html"
+
+
+class Hebergement(TemplateView):
+    template_name = "website/hebergement.html"
+
+
+class Marketing(TemplateView):
+    template_name = "website/marketing.html"
